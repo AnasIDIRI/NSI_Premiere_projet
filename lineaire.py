ce9afb59268f3dba88a3a6c6f45d0736067fcb7a
AnasIDIRI <104441075+AnasIDIRI@users.noreply.github.com>
2022-04-26 16:30:26 +0200

    Update lineaire.py

--- lineaire.py
+++ lineaire.py
@@ -5,8 +5,8 @@ def linéaireGen():
     """
     
     Fonction qui génere deux nombre aléatoire
-    a doit toujours etre supérieur a 0
-    donc nous effectuons un do while
+    "a" doit toujours être supérieur à 0
+    donc nous effectuons une sorte de do while
 
     """
     
@@ -23,8 +23,8 @@ def linéaireGen():
 
 def Reponse(a, question):
     
-    """calculer la réponse d'une fonction linéaire"""
+    """calculer la solution d'une fonction linéaire"""
 
     x = question / a
 
-    return round(x, 2) # Arrondir au centième
+    return round(x, 2) # On arrondit au centième
